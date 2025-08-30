731769501574949d3732cd27a507fe02c1630516
raguladhithyam <mra20031006@gmail.com>
2025-08-30 14:12:01 +0530

    deployment updates

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
